616cd5c38971ac8b38d1a54aac03cb4625f0aaba
antoniogambogi <antoniogambogi88@gmail.com>
2021-01-10 20:06:15 -0300

    Implementando função para o Dino subir e descer

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ const dino = document.querySelector('.dino');
 
 function heandleKeyUp(event) {
     if (event.keyCode === 32) {
-        console.log('Teste tecla espaço')
+        jump();
     }
+}
+
+function jump() {
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+            //Descendo o Dino
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px'
+                }
+            }, 20);
+        } else {
+            //Subindo o Dino
+            position += 20;
+
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener('keyup', heandleKeyUp);
